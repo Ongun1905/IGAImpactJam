3c5d73864cf50ebcb44a86d9d5ae683826566aef
ThanosTsalafoutas <a.tsalafoutas@student.tue.nl>
2022-02-12 16:17:23 +0100

    Revert "Merge pull request #6 from Ongun1905/ShootingPipes"
    This reverts commit 5cefc3d55223c4a993274414d61e049d4625a3dc, reversing
    changes made to 044c2853f7728aaaf54da83d07026483129a92d5.

---
+++ NeonClimate/Assets/PlayerController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 velocity;
+    public float speed;
+    public int jumpHeight;
+    public LayerMask whatIsGround;
+    private Rigidbody2D rigidbody;
+    private float lastGroundedVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = transform.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Jump");
+
+        var velocity = rigidbody.velocity;
+        var grounded = IsGrounded();
+        if (grounded)
+        {
+            velocity.x = horizontal * speed;
+            lastGroundedVelocity = velocity.x;
+        } else
+        {
+            velocity.x += horizontal * speed / 16;
+            if (velocity.x < -speed)
+            {
+                velocity.x = -speed;
+            }
+            else if (velocity.x > speed)
+            {
+                velocity.x = speed;
+            }
+        }
+
+        if (grounded && vertical > 0.0)
+        {
+            velocity.y = jumpHeight;
+        }
+
+        rigidbody.velocity = velocity;
+    }
+
+    bool IsGrounded()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down, 0.51f, whatIsGround);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(transform.position, Vector2.down, Color.green, 1);
+    }
+}
