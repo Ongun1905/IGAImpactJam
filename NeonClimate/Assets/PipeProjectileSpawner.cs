4c49fd769156d3aef693b7bcda91f1f8acbbc0fd
ThanosTsalafoutas <a.tsalafoutas@student.tue.nl>
2022-02-12 14:13:39 +0100

    Added a Pipe

---
+++ NeonClimate/Assets/PipeProjectileSpawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeProjectileSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject prefabPipeProjectile;
+    [SerializeField] private Transform spawnPosPipe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
